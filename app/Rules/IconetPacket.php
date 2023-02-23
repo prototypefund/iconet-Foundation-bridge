44e4aa161aa1e477ae884e6f4fb62eec5212cca8
Martin Miller <martin.mil@web.de>
2023-02-23 22:10:03 +0100

    Relax validation rules for iconet packets
    This is a workaround since it seems harder to strictly follow the packet format in mastodon.
    Ideally this should be reverted when mastodon complies.
    Problems at the moment are:
    - hyphens in json keys are missing
    - the 'to' field is an array since it mirrors the mentioned users api.
    - the post id should be part of the payload.

--- app/Rules/IconetPacket.php
+++ app/Rules/IconetPacket.php
@@ -11,16 +11,17 @@ class IconetPacket implements InvokableRule
         '@context' => "required|in:https://ns.iconet-foundation.org#",
         '@type' => "required|in:Packet",
         '@id' => "required|url",
-        'actor' => "required", // can be string or array
-        'to' => "required|string",
+        'actor' => "required", // TODO can be string or array
+        'to' => "required", // TODO can be string or array
         'interpreterManifests' => "required|array|min:1",
-        'interpreterManifests.*.manifestUri' => "required|url",
+        'interpreterManifests.*.manifestUri' => "required|string", //TODO should be url
         'interpreterManifests.*.sourceTypes' => "required|array",
         'interpreterManifests.*.targetTypes' => "required|array",
-        'interpreterManifests.*.sha-512' => "required|string",
+        'interpreterManifests.*.sha-512' => "string", // TODO required
+        'interpreterManifests.*.sha512' => "string",  // TODO serialize hyphens in mastodons note_serializer
         'content' => "required|array|min:1",
         'content.*.packetType' => "required|string",
-        'content.*.payload' => "required|string",
+        'content.*.payload' => "required", // TODO should be string?
     ];
 
     /**
